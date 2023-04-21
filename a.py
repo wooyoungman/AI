864d1688c117bee29525c4e22f37fb357e4c8fc1
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-04-21 21:15:02 +0900

    Update a.py

--- a.py
+++ a.py
@@ -4,6 +4,12 @@ import os
 jpg_files = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.jpg')]
 
 # 파일명에서 .jpg 확장자를 제외한 이름만 추출하여 출력
+filename_list = []
 for jpg_file in jpg_files:
     filename_without_extension = os.path.splitext(jpg_file)[0]
-    print(filename_without_extension)
+    filename_list.append(filename_without_extension)
+
+# 추출한 파일명 리스트를 텍스트 파일에 저장
+with open('filename_list.txt', 'w') as f:
+    for filename in filename_list:
+        f.write("%s\n" % filename)
